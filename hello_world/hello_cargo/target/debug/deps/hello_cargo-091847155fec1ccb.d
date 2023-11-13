13eafa1c62879a66790e6ac4c5e80d5d93c7d3f0
shas99 <pshasvathan1999@gmail.com>
2023-11-13 21:11:16 +0530

    rust getting started

---
+++ hello_world/hello_cargo/target/debug/deps/hello_cargo-091847155fec1ccb.d
@@ -0,0 +1,5 @@
+/home/shasvathan/rustProject/hello_world/hello_cargo/target/debug/deps/hello_cargo-091847155fec1ccb: src/main.rs
+
+/home/shasvathan/rustProject/hello_world/hello_cargo/target/debug/deps/hello_cargo-091847155fec1ccb.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/shasvathan/rustProject/hello_world/hello_cargo/target/debug/hello_cargo: /home/shasvathan/rustProject/hello_world/hello_cargo/src/main.rs
